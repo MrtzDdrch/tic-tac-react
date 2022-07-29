97602e98a34e62c4d2dd297c4adfa9352b8e1cc2
ARC\mdiedrich <diedrich.moritz@arc-greenlab.de>
2022-07-29 16:28:23 +0200

    turned the squares into controlled components

--- src/index.js
+++ src/index.js
@@ -3,30 +3,43 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 class Square extends React.Component {
-    
-    constructor(props){
-        super(props);
-        this.state = {
-            value: null,
-        }
-    }
-
     render() {
       return (
-        <button className="square" onClick={function(){alert("lkjh")}} >
-          {this.props.value}
+        <button 
+            className="square" 
+            onClick={() => {this.props.onClick()}}
+        >
+            {this.props.value}
         </button>
       );
     }
 }
   
 class Board extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
+
+    handleClick(i){
+        const squares = this.state.squares.slice();
+        squares[i] = "x";
+        this.setState({squares: squares});
+    }
+    
     renderSquare(i) {
-        return <Square value={i} />;
+        return (
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = '';
 
         return (
             <div>
@@ -72,4 +85,10 @@ class Game extends React.Component {
   const root = ReactDOM.createRoot(document.getElementById("root"));
   root.render(<Game />);
   
-  
+  // Would it be possible to "mimick" an SPA by doing it all server side and updating only individual components? Would that work with JS disabled?
+  // Why do I always need to call on super() with the props when using classes in ES6?
+
+  // "By calling this.setState from an onClick handler in the Square’s render method, we tell React to re-render that Square whenever its <button> is clicked."
+  // I want to see documentation about all the functions that come with a React component
+
+  // Last stop: Function Components
